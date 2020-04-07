95d1ecd4356afbdc23fe59308ffbd9f3b7344e97
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-04-07 23:24:09 +0530

    attack

--- Assets/Scripts/actor.cs
+++ Assets/Scripts/actor.cs
@@ -11,10 +11,13 @@ public class actor : MonoBehaviour
     public int maxHealth;
     int currentHealth;
     public GameObject sword;
+    public float thrustPower;
+    public bool canMove;
 
     // Start is called before the first frame update
     void Start()
     {
+        canMove = true;
         anim = GetComponent<Animator>();
         currentHealth = maxHealth;
         getHealth();
@@ -47,20 +50,39 @@ public class actor : MonoBehaviour
 
     void Attack()
     {
+        canMove = false;
         GameObject newSword = Instantiate(sword, transform.position, sword.transform.rotation);
-        int swordDir = anim.GetInteger("dir");
 
+        #region //Sword Direction
+        int swordDir = anim.GetInteger("dir");
         if(swordDir==0)
-        {  newSword.transform.Rotate(0, 0, 0); }
+        {  
+            newSword.transform.Rotate(0, 0, 0);
+            newSword.GetComponent<Rigidbody2D>().AddForce(Vector2.up * thrustPower);
+        }
         else if(swordDir==1)
-        { newSword.transform.Rotate(0, 0, 180); }
+        { 
+            newSword.transform.Rotate(0, 0, 180);
+            newSword.GetComponent<Rigidbody2D>().AddForce(Vector2.up * -thrustPower);
+        }
         else if (swordDir == 2)
-        { newSword.transform.Rotate(0, 0, 90); }
+        { 
+            newSword.transform.Rotate(0, 0, 90);
+            newSword.GetComponent<Rigidbody2D>().AddForce(Vector2.right * -thrustPower);
+        }
         else if (swordDir == 3)
-        { newSword.transform.Rotate(0, 0, -90); }
+        {
+            newSword.transform.Rotate(0, 0, -90);
+            newSword.GetComponent<Rigidbody2D>().AddForce(Vector2.right * thrustPower);
+        }
+        #endregion
+        
     }
     void Movement()
     {
+        if (!canMove)
+            return;
+
         if(Input.GetKey(KeyCode.W))
         { transform.Translate(0, speed * Time.deltaTime, 0); anim.SetInteger("dir", 0); anim.speed = 1; }
 
