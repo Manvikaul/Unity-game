e2b2760472627e3ae6aa117019094e42a1f2945d
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-04-07 13:25:39 +0530

    player scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +13,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Movement();
-    }
-
-    void Movement()
-    {
-
+        
     }
 }
