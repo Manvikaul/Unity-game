5ea7d02bb15e6f86ab2756294bb23f003c20c12d
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-04-09 19:50:18 +0530

    crab movement

--- Assets/Scripts/crab.cs
+++ Assets/Scripts/crab.cs
@@ -6,17 +6,56 @@ public class crab : MonoBehaviour
 {
     public int health;
     public GameObject particleEffect;
+    SpriteRenderer spriteRenderer;
+    int direction;
+    float timer = 1.5f;
+    public float speed;
+    public Sprite facingUp;
+    public Sprite facingDown;
+    public Sprite facingRight;
+    public Sprite facingLeft;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer.GetComponent<SpriteRenderer>();
+        direction = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if(timer<=0)
+        {
+            timer = 1.5f;
+            direction = Random.Range(0, 3);
+        }
+        Movement();
+    }
+
+    public void Movement()
+    {
+        if(direction==0)
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+            spriteRenderer.sprite = facingDown;
+        }
+        else if(direction==1)
+        {
+            transform.Translate( -speed * Time.deltaTime,0, 0);
+            spriteRenderer.sprite = facingLeft;
+        }
+        else if (direction == 2)
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+            spriteRenderer.sprite = facingRight;
+        }
+        else if (direction == 3)
+        {
+            transform.Translate(0,speed * Time.deltaTime, 0);
+            spriteRenderer.sprite = facingUp;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
