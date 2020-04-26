dc376340eee8e94c276391485f70cd486db7acef
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-04-26 19:34:32 +0530

    level transition

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int sceneIndex;
 
-    // Update is called once per frame
-    void Update()
+   void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if(col.gameObject.tag=="Player")
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
+   
 }
