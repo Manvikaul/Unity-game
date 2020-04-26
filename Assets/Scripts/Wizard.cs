0559758ca27df03046062e9921957454e027fcde
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-04-26 12:05:34 +0530

    boss script

--- Assets/Scripts/Wizard.cs
+++ Assets/Scripts/Wizard.cs
@@ -4,15 +4,162 @@ using UnityEngine;
 
 public class Wizard : MonoBehaviour
 {
+    Animator anim;
+    public float speed;
+    public int dir;
+    float dirTimer = 0.7f;
+    public int health;
+    public GameObject deathParticle;
+    bool canAttack;
+    float attackTimer = 2f;
+    public float thrustPower;
+    public GameObject projectile;
+    float changeTimer = 0.2f;
+    bool shouldChange;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        canAttack = false;
+        shouldChange = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dirTimer -= Time.deltaTime;
+        if (dirTimer <= 0)
+        {
+            dirTimer = 0.7f;
+            dir = Random.Range(0, 4);
+        }
+        Movement();
+
+        attackTimer -= Time.deltaTime;
+        if (attackTimer <= 0)
+        {
+            attackTimer = 2f;
+            canAttack = true;
+        }
+
+        Attack();
+
+        if (shouldChange)
+        {
+            changeTimer -= Time.deltaTime;
+            if (changeTimer <= 0)
+            {
+                shouldChange = false;
+                changeTimer = 0.2f;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        if (!canAttack)
+            return;
+
+        canAttack = false;
+        if (dir == 0)
+        {
+            GameObject newProjectile = Instantiate(projectile, transform.position, transform.rotation);
+            newProjectile.GetComponent<Rigidbody2D>().AddForce(Vector2.up * thrustPower);
+        }
+        else if (dir == 1)
+        {
+            GameObject newProjectile = Instantiate(projectile, transform.position, transform.rotation);
+            newProjectile.GetComponent<Rigidbody2D>().AddForce(Vector2.right * -thrustPower);
+        }
+        else if (dir == 2)
+        {
+            GameObject newProjectile = Instantiate(projectile, transform.position, transform.rotation);
+            newProjectile.GetComponent<Rigidbody2D>().AddForce(Vector2.up * -thrustPower);
+        }
+        else if (dir == 3)
+        {
+            GameObject newProjectile = Instantiate(projectile, transform.position, transform.rotation);
+            newProjectile.GetComponent<Rigidbody2D>().AddForce(Vector2.right * thrustPower);
+        }
+
+    }
+
+    void Movement()
+    {
+        if (dir == 0)
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+            anim.SetInteger("dir", dir);
+        }
+        else if (dir == 1)
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+            anim.SetInteger("dir", dir);
+        }
+        else if (dir == 2)
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+            anim.SetInteger("dir", dir);
+        }
+        else if (dir == 3)
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+            anim.SetInteger("dir", dir);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Sword")
+        {
+            health--;
+            col.gameObject.GetComponent<Sword>().CreateParticle();
+            GameObject.FindGameObjectWithTag("Player").GetComponent<actor>().canAttack = true;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<actor>().canMove = true;
+            Destroy(col.gameObject);
+            if (health <= 0)
+            {
+                Instantiate(deathParticle, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            health--;
+            if (!col.gameObject.GetComponent<actor>().iniframes)
+            {
+                col.gameObject.GetComponent<actor>().currentHealth--;
+                col.gameObject.GetComponent<actor>().iniframes = true;
+            }
+            col.gameObject.GetComponent<actor>().currentHealth--;
+            if (health <= 0)
+            {
+                Instantiate(deathParticle, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+        if (col.gameObject.tag == "Wall")
+        {
+            if (shouldChange)
+                return;
+
+            if (dir == 0)
+                dir = 2;
+            else if (dir == 1)
+                dir = 3;
+            else if (dir == 2)
+                dir = 0;
+            else if (dir == 3)
+                dir = 1;
+
+            shouldChange = true;
+
+        }
     }
 }
+
